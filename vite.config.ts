36009640bed842ccdc67d16343445bff13ae2cc4
Heena <hkbaig5@gmail.com>
2025-08-22 13:14:31 +0530

    test: added unit tests for core functionality - Week 6

--- vite.config.ts
+++ vite.config.ts
@@ -12,3 +12,7 @@ export default defineConfig({
 // Development update - Week 3
 // Date: 2025-08-04 13:21
 // Saree customization feature improvements
+
+// Development update - Week 6
+// Date: 2025-08-22 13:14
+// Saree customization feature improvements
