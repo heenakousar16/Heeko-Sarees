cd2326f541b60151f2babfc3b35d734082ed6a06
Heena <hkbaig5@gmail.com>
2025-08-25 10:10:48 +0530

    content: added new fabric patterns - Week 6

--- src/components/FabricSelector.tsx
+++ src/components/FabricSelector.tsx
@@ -2,3 +2,7 @@
 // Development update - Week 1
 // Date: 2025-07-19 10:14
 // Saree customization feature improvements
+
+// Development update - Week 6
+// Date: 2025-08-25 10:10
+// Saree customization feature improvements
