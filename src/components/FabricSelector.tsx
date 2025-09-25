250588077f460a2d0328abbf915cb36c2a3591c3
Heena <hkbaig5@gmail.com>
2025-09-25 12:14:07 +0530

    fix: resolved 3D preview rendering issues - Week 11

--- src/components/FabricSelector.tsx
+++ src/components/FabricSelector.tsx
@@ -6,3 +6,7 @@
 // Development update - Week 6
 // Date: 2025-08-25 10:10
 // Saree customization feature improvements
+
+// Development update - Week 11
+// Date: 2025-09-25 12:14
+// Saree customization feature improvements
